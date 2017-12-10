00de8c0d723c26a2bb540aeb4d4e68d81e4085db
mkvtvseries <mkvtvseries@users.noreply.github.com>
2017-12-10 18:48:03 +0530

    Update App.java

--- src/main/java/wf/doyle/WarpWallet/App.java
+++ src/main/java/wf/doyle/WarpWallet/App.java
@@ -17,6 +17,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
+import java.util.Random;
 import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -70,7 +71,7 @@ public class App {
         f.setAccessible(true);
         System.out.println("I'm using " + (f.getBoolean(null) ? "native libs" : "java libs") + " for Scrypt.");
 
-        final SecureRandom random = new SecureRandom(); // hopefully more randomness given to us by the OS.
+        final Random random = new Random(); // hopefully more randomness given to us by the OS.
 
         final int length = Integer.parseInt(args[2]);
 
